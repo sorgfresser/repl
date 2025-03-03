60287675033f9d828ac34f2a7ad62385fca2ef06
Auguste Poiroux <augustepoiroux@gmail.com>
2025-03-03 21:00:58 +0100

    v4.16.0-rc1

--- test.sh
+++ test.sh
@@ -41,6 +41,6 @@ for infile in $IN_DIR/*.in; do
 
 done
 
-# Run the Mathlib tests
-cp lean-toolchain test/Mathlib/
-cd test/Mathlib/ && ./test.sh
+# # Run the Mathlib tests
+# cp lean-toolchain test/Mathlib/
+# cd test/Mathlib/ && ./test.sh
